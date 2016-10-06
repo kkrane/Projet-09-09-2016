180704588e1c31719a676e55172f9a0fa29864d1
kkrane <cloarec.j95@gmail.com>
2016-10-06 21:36:00 +0200

    Add files via upload

---
+++ docutil.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>La documentation utilisateur vous permettra de vous dirigez sur notre site.</h1>
+        <p>Si vous êtes ici, c'est que vous nous avez choisi pour garer votre voiture. Vous avez donc aussi été valider par l'administrateur.</p><br/>
+        <p>Pour demander une place, vous devez utiliser le bouton bleu. Si une place est disponible, elle vous sera attribuer automatiquement. Dans le cas contraire, vous serez placer sur une liste d'attente. Vous pourrez vous connecter afin de voir votre avancer dans la liste d'attente. Le temps d'attente estimé est d'une semaine, et peut prendre jusqu'à un mois dans les périodes de vacances. </p>
+        
+    </div>
+</div>
+
+@endsection
